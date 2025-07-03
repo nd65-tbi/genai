d3d5b25f7817a14906623e97dbee1eb88a9695e2
nd65-tbi <51125921+nd65-tbi@users.noreply.github.com>
2025-07-02 23:15:23 -0500

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -1,6 +1,6 @@
 // credentials.js
 const users = [
   { username: "murthy", password: "welcome123" },
-  { username: "Guest", password: "@Admin123" },
+  { username: "Guest", password: "Admin123" },
   { username: "user2", password: "password2" }
 ];
